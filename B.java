2328a7b9a30c18ab7834a6fb1652b300c5a519db
THIRU <THIRU@192.168.0.9>
2016-09-18 17:20:37 +0530

    this is from local repository

--- B.java
+++ B.java
@@ -4,7 +4,5 @@ Public static void main(String args[])
 {
 System.out.println("this is lucky local repository");
 System.out.println("added one more stmt in local repository");
-System.out.println("this is lucky from remote repository");
-System.out.println("this is lucky branching from local repository");
 }
 }
