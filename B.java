d1188b98c4dbffa01c764856b5970d46552933a3
thiru venkata sai <thiru venkata sai>
2016-09-18 17:10:35 +0530

    this is branching from remote repository

--- B.java
+++ B.java
@@ -5,6 +5,6 @@ Public static void main(String args[])
 System.out.println("this is lucky local repository");
 System.out.println("added one more stmt in local repository");
 System.out.println("this is lucky from remote repository");
-System.out.println("this is lucky branching from local repository");
+System.out.println("this is lucky branching from remote repository");
 }
 }
